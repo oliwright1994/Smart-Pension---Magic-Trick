274397dcc2ec2d52902285053903fca105bb1c03
Oli Wright <oli.wright@blueyonder.co.uk>
2019-11-13 16:07:30 +0000

    added yarn sass script to package.json

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const suits = ["hearts", "spades", "diamonds", "clubs"];
-const cardsWrapper = document.querySelector(".cards-wrapper");
+const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
+const cardsWrapper = document.querySelector('.cards-wrapper');
 
 function createCards() {
   const cards = [];
@@ -17,12 +17,12 @@ function createCards() {
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
     const positionFromLeft = i * 30;
-    const cardElement = document.createElement("div");
-    cardElement.setAttribute("data-value", card.value);
-    cardElement.classList.add("card", `${card.suit}-${card.value}`);
+    const cardElement = document.createElement('div');
+    cardElement.setAttribute('data-value', card.value);
+    cardElement.classList.add('card', `${card.suit}-${card.value}`);
     cardElement.style.left = `${positionFromLeft}px`;
-    const cardFace = document.createElement("div");
-    cardFace.classList.add("card-face");
+    const cardFace = document.createElement('div');
+    cardFace.classList.add('card-face');
     cardFace.innerHTML = `<p class='top'>
     <span class='value'></span>
     <span class='suit'></span>
@@ -33,9 +33,9 @@ function createCards() {
     <span class='suit'></span>
     </p>`;
     cardElement.appendChild(cardFace);
-    const logo = document.createElement("img");
-    logo.src = "./assets/smart-logo.svg";
-    logo.style.display = "none";
+    const logo = document.createElement('img');
+    logo.src = './assets/smart-logo.svg';
+    logo.style.display = 'none';
     cardElement.appendChild(logo);
     cardsWrapper.append(cardElement);
   });
@@ -43,18 +43,18 @@ function createCards() {
 
 // Function that toggles the class 'shuffling' on the cards wrapper to animate 'stacking' the cards
 function toggleStackedCards() {
-  cardsWrapper.classList.add("shuffling");
+  cardsWrapper.classList.add('shuffling');
   // Timeout to remove the 'shuffling' class to allow the cards to spread out according to
   // their 'left' css property
   setTimeout(() => {
-    cardsWrapper.classList.remove("shuffling");
+    cardsWrapper.classList.remove('shuffling');
   }, 1500);
 }
 // Function to shuffle cards into a random order
 function shuffleCards() {
   // Call to function to 'stack' cards
   toggleStackedCards();
-  const cards = document.querySelectorAll(".card");
+  const cards = document.querySelectorAll('.card');
 
   // Create an array of values from 0-51 to represent cards in a deck
   const orderedDeck = Array.from(new Array(52), (x, i) => i);
@@ -80,7 +80,7 @@ function shuffleCards() {
 
   // Refetch the array of all cards in the DOM, now shuffled, and correctly updates their
   // absolute positioning to appear spread out again once the cards are unstacked
-  const shuffledCards = document.querySelectorAll(".card");
+  const shuffledCards = document.querySelectorAll('.card');
   shuffledCards.forEach((card, j) => {
     card.style.left = `${j * 30}px`;
   });
@@ -88,13 +88,13 @@ function shuffleCards() {
 
 // Function to toggle cards between face up and face down (adding and removing 'hidden' class)
 function toggleCards() {
-  cardsWrapper.classList.toggle("hidden");
+  cardsWrapper.classList.toggle('hidden');
 }
 
 // Function to return shuffled cards to ordered by suit
 function magicTrick() {
   toggleStackedCards();
-  const shuffledCards = document.querySelectorAll(".card");
+  const shuffledCards = document.querySelectorAll('.card');
 
   const allCardClasses = [];
   // Create an array with objects containing the value and the suit of each card
@@ -118,21 +118,21 @@ function magicTrick() {
 
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
-  const startButton = document.getElementById("start-game");
+  const startButton = document.getElementById('start-game');
   startButton.remove();
-  const buttonWrapper = document.querySelector(".btn-wrapper");
+  const buttonWrapper = document.querySelector('.btn-wrapper');
   const gameButtons = [
-    { text: "Shuffle", function: shuffleCards },
-    { text: "Show/Hide", function: toggleCards },
-    { text: "Magic", function: magicTrick }
+    { text: 'Shuffle', function: shuffleCards },
+    { text: 'Show/Hide', function: toggleCards },
+    { text: 'Magic', function: magicTrick }
   ];
   gameButtons.forEach(gameButton => {
-    const newButton = document.createElement("button");
-    newButton.type = "button";
+    const newButton = document.createElement('button');
+    newButton.type = 'button';
     newButton.id = gameButton;
-    newButton.classList.add("btn", "btn-lg", "btn-secondary", "mx-2");
+    newButton.classList.add('btn', 'btn-lg', 'btn-secondary', 'mx-2');
     newButton.innerText = gameButton.text;
-    newButton.addEventListener("click", gameButton.function);
+    newButton.addEventListener('click', gameButton.function);
     buttonWrapper.append(newButton);
   });
 }
@@ -143,4 +143,4 @@ function startGame() {
   createCards();
 }
 
-document.getElementById("start-game").addEventListener("click", startGame);
+document.getElementById('start-game').addEventListener('click', startGame);
